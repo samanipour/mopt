42eced1eb0b97254062920242fddb9a145ee03d2
samanipour <alisamanipour.official@gmail.com>
2024-03-26 00:34:44 +0330

    feat (problems): develop objective function and solution space data structure for Stock Prediction Problem

---
+++ src/problems/stockPrediction/StockPrediction.java
@@ -0,0 +1,42 @@
+
+/*
+ * Developer  : Ali Samanipour
+ * Contact    : AliSamanipour.Official@Gmail.com
+ * GitHub     : https://github.com/samanipour/
+ * Description: An objective function and Solution Space for Stock Prediction problem
+ * 
+ */
+package problems.stockPrediction;
+
+import interfaces.IObjectiveFunction;
+
+public class StockPrediction implements IObjectiveFunction<double[]>{
+    double[] actualPrices;
+    double[] predictedPrice;
+
+    public StockPrediction(double[] actualPricesArg){
+        super();
+        this.actualPrices=actualPricesArg;
+    }
+
+    @Override
+    public double compute(double[] x) {
+        this.predictedPrice=x;
+        double mse = calculateMSE(x);
+        return mse;
+    }
+
+    private double calculateMSE(double[] solutionCandidate) {
+        // Calculate the MSE between predicted and actual stock prices
+        double sumSquaredErrors = 0.0;
+        int n = actualPrices.length;
+
+        for (int i = 0; i < n; i++) {
+            double error = this.predictedPrice[i] - actualPrices[i];
+            sumSquaredErrors += Math.pow(error, 2);
+        }
+
+        double mse = sumSquaredErrors / n;
+        return mse;
+    }
+}
